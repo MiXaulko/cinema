6dfa43a222e9ca575f9ee982546db5fffe47bb16
MiXaulko <abc1121@mail.ru>
2020-05-14 18:36:55 +0400

    Первоначальный коммит

---
+++ src/test/kotlin/ru/kirillov/cinema/CinemaApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.kirillov.cinema
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
